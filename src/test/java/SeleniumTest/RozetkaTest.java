00b6e94f98b2141e57452f0f2cce5270e7c5aee3
Yura Kochubailo <yurii.kochubailo@gmail.com>
2023-04-26 12:37:33 +0200

    solve

---
+++ src/test/java/SeleniumTest/RozetkaTest.java
@@ -0,0 +1,35 @@
+package SeleniumTest;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.annotations.*;
+
+public class RozetkaTest {
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Yura Kochubailo.000\\Desktop\\Selenium WebDriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void testSearch() {
+        driver.get("https://rozetka.com.ua/");
+        WebElement searchBar = driver.findElement(By.xpath("//input[@name='search']"));
+        searchBar.click();
+        searchBar.sendKeys("iphone 13", Keys.ENTER);
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
